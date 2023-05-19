26648ff493d52c2e5b0feb149fbb4c756e4a05a0
Setu Gupta <setu.gupta.2020@gmail.com>
2023-05-19 18:31:46 +0530

    Added scripts to make plots with larger font sizes
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   simulator/plotter_poster_accuracy.py
    	new file:   trace_analysis/analyze_branch_freq_poster.py
    	new file:   trace_analysis/plot_trace_poster.py

---
+++ simulator/plotter_poster_accuracy.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+if len(sys.argv) <= 1:
+    print("Usage: The first argument is the results file")
+
+names = []
+values = []
+names_RMA = []
+values_RMA = []
+name_best = []
+value_best = []
+with open(sys.argv[1], 'r') as results:
+    for line in results.readlines():
+        name, value = line.split(':')
+        name = name.strip().split("MPKI for ")[-1].replace("with", "with\n")
+
+        value = 100 - ((1000 - float(value.strip()))/10)
+        if "MarkerBP" in name:
+            if "default not taken" in name:
+                name_best.append(name)
+                value_best.append(value)
+            else:
+                names_RMA.append(name)
+                values_RMA.append(value)
+        else:
+            names.append(name)
+            values.append(value)
+
+all_labels = names + names_RMA + name_best
+all_values = values + values_RMA + value_best
+all_label_positions = np.arange(len(all_labels))
+plt.title("Branch misprediction rate for different branch predictors", fontsize=22)
+plt.xlabel("% Mispredictions", fontsize=22)
+plt.ylabel("Configurations", fontsize=22)
+plt.barh(all_label_positions[:len(names)], values, color='r')
+plt.barh(all_label_positions[len(names):len(names) + len(names_RMA)], values_RMA, color='b')
+plt.barh(all_label_positions[len(names) + len(names_RMA):], value_best, color='g')
+plt.yticks(all_label_positions, labels=all_labels, fontsize=18)
+plt.xticks(fontsize=18)
+plt.show()
